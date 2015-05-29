b1bb8734aca46d7272671013707ab0a7ab5b3489
Richard Ashcraft <rla.business@gmail.com>
2015-05-28 23:18:22 -0500

    Created barebones REST project

---
+++ src/main/java/com/ashcr/mptc/api/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.ashcr.mptc.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @RequestMapping("/healthcheck")
+    public String healthCheck() {
+        return "Health check ok!";
+    }
+}
